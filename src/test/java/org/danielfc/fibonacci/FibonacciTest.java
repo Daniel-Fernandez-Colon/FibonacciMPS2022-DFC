f469486c3ea935c27c6e963b4ce5178475c397e6
danielfc <0619988275@uma.es>
2022-03-07 18:50:29 +0100

    Test para numero 0

--- src/test/java/org/danielfc/fibonacci/FibonacciTest.java
+++ src/test/java/org/danielfc/fibonacci/FibonacciTest.java
@@ -1,8 +1,42 @@
 package org.danielfc.fibonacci;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * fibonacci 0->0
+ * fibonacci 1->1
+ * fibonacci 2->1
+ * fibonacci 3->2
+ * fibonacci 4->3
+ * fibonacci 10->55
+ * fibonacci negative number->error
+ */
+
+
+
 class FibonacciTest {
+
+    private Fibonacci fibonacci;
+    @BeforeEach
+    private void setup(){
+        fibonacci = new Fibonacci();
+    }
+
+    @AfterEach
+    private void finish(){
+        fibonacci = null;
+    }
+
+    @Test
+    public void shouldComputeReturnZeroIfTheNumberIsZero(){
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.compute(0);
+        assertEquals(expectedValue,obtainedValue);
+    }
 
 
 
